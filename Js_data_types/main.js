95a8011d640ff6251ced372d531d07b4c4528f64
eKICH-Programming <83930090+eKICH-Programming@users.noreply.github.com>
2021-07-22 01:40:53 +0300

    Add files via upload

---
+++ Js_data_types/main.js
@@ -0,0 +1,45 @@
+// This is single line comment
+
+/* 
+This is multi line comment
+*/
+
+// variable Key words
+// var
+// let
+// const
+// Javascript Data types
+
+// String
+let firstname = 'James';
+let secondname = 'Martin';
+
+// Number
+let x = 10;
+const PI = 3.14;
+let y = 3.56;
+
+// Boolean
+let warm = false;
+let cold = true;
+
+// Undefined
+let lastname;
+let market = undefined;
+
+// Null
+let multi = null;
+let g = '';
+
+// Javascript variables
+
+let house = 'East wing';
+let _cartype = 'Ferrari';
+let $age = 30;
+let first = 'Jaquar';
+let first_car = 'Honda Civic';
+let FIRST_CAR = 'Ford';
+
+console.log(house);
+// window.alert(_cartype);
+alert($age);
